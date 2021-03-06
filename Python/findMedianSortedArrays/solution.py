666403b9a9c1433a5e14fe1cc389a22e126b2eb8
GABRIEL <angelgab43>
2021-03-06 12:44:49 +0300

    Updated solution

--- Python/findMedianSortedArrays/solution.py
+++ Python/findMedianSortedArrays/solution.py
@@ -1,17 +1,11 @@
-class Solution():
-    def findMedianSortedArrays(self, nums1, nums2):
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         nums = sorted(nums1+nums2)
         length = len(nums)
-        print(length)
-        print(nums)
         if length%2 == 0 and len(nums1) <= 1000 and len(nums2) <= 1000:
-            print("indice1: ",int(length/2)-1)
-            print("indice2: ",int(length/2))
-            print(nums[int(length/2)-1])
-            print(nums[int(length/2)])
-            print((nums[int(length/2)-1] + nums[int(length/2)])/2)
+            return (nums[int(length/2)-1] + nums[int(length/2)])/2
         else:
-            print(nums[length//2])
+            return nums[length//2]
 
 sol= Solution()
 # sol.findMedianSortedArrays([0,0,0,0,0],[-1,0,0,0,0,0,1])
