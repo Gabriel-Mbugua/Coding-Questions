b909008308e0b26d67ff5018d841f50384010c21
GABRIEL <angelgab43>
2021-06-01 10:54:35 +0300

    Added Permutations Solution

---
+++ Python/Permutations/solution.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(permutations(nums))
+
+        # return nums
+
+print(Solution().permute([1,2,3]))
